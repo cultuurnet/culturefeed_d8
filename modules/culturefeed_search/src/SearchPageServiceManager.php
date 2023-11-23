f6dba6b54a5547214998f3cc0784e58c311fe419
Wouter Vanelslander <wouter.vanelslander@intracto.com>
2023-11-23 16:03:20 +0100

    PUBLIQ-952: Deprecated _serviceId property

--- modules/culturefeed_search/src/SearchPageServiceManager.php
+++ modules/culturefeed_search/src/SearchPageServiceManager.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\culturefeed_search;
 
+use Drupal\Component\DependencyInjection\ReverseContainer;
+use Drupal\Core\DrupalKernelInterface;
+
 /**
  * Provides a SearchPageServiceManager to manage the known search page services.
  */
@@ -12,14 +15,28 @@ class SearchPageServiceManager implements SearchPageServiceManagerInterface {
    *
    * @var array
    */
-  protected $searchPageServices;
+  protected array $searchPageServices = [];
+
+  /**
+   * The ReverseContainer service.
+   *
+   * @var ReverseContainer
+   */
+  protected ReverseContainer $reverseContainer;
+
+  public function __construct(ReverseContainer $reverseContainer) {
+    $this->reverseContainer = $reverseContainer;
+  }
 
   /**
    * {@inheritdoc}
    */
   public function addSearchPage(SearchPageServiceInterface $searchPageService, $priority = 0): SearchPageServiceManagerInterface {
-    $serviceId = $searchPageService->_serviceId;
-    $this->searchPageServices[$serviceId] = $searchPageService;
+    $serviceId = $this->reverseContainer->getId($searchPageService);
+    if (NULL !== $serviceId) {
+      $this->searchPageServices[$serviceId] = $searchPageService;
+    }
+
     return $this;
   }
 
